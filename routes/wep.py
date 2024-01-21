77763e380695850d2cf57adaa096a054450ced3f
RiwRiwara <awirut2629@gmail.com>
2024-01-21 14:13:26 +0700

    first commit

---
+++ routes/wep.py
@@ -0,0 +1,32 @@
+from . import defaultAPI
+from flask import  jsonify, render_template, request, session, redirect, url_for, send_file, send_from_directory
+import json
+from config.db import db
+
+
+@defaultAPI.route('/testapi', methods=['GET'])
+def api_entry():
+    collection_names = db.list_collection_names()
+    response = {
+        'data': "API Running",
+        'collection_names': collection_names
+    }
+    return jsonify(response)
+
+
+@defaultAPI.route('/')
+@defaultAPI.route('/index')
+@defaultAPI.route('/home')
+def index():
+    return render_template('index.html')
+
+@defaultAPI.route('/img/<filename>')
+def send_img(filename):
+    return send_from_directory('./static/img', filename)
+@defaultAPI.route('/robots.txt')
+def send_robots():
+    return send_from_directory('./static', 'robots.txt')
+
+@defaultAPI.errorhandler(404)
+def page_not_found(e):
+    return redirect(url_for('index'))
